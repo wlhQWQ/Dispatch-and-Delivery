c49d5d3a720cc9db464d2f9ee8d0ec011dfec644
Harry Kang <40790537+kanglinde@users.noreply.github.com>
2025-12-29 09:49:25 -0800

    added signup,login,logout,session management by Redis

---
+++ backend/Dispatch-and-Delivery/src/main/java/com/flagcamp/dispatchanddelivery/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.flagcamp.dispatchanddelivery.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // clean previous session IDs
+    @Bean
+    public CommandLineRunner clearRedis(RedisConnectionFactory factory) {
+        return args -> {
+            System.out.println("Cleaning Redis session data...");
+            factory.getConnection().serverCommands().flushAll();
+        };
+    }
+}
---
+++ backend/Dispatch-and-Delivery/src/main/java/com/flagcamp/dispatchanddelivery/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.flagcamp.dispatchanddelivery.controller;
+
+import com.flagcamp.dispatchanddelivery.model.RegisterBody;
+import com.flagcamp.dispatchanddelivery.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void signUp(@RequestBody RegisterBody body) {
+        userService.signUp(body.email(), body.password());
+    }
+}
